873bb0d9a6ea4fc0344c714648db27f59f44effa
HieuBuiLe <94900815+HieuBuiLe@users.noreply.github.com>
2021-12-08 16:06:52 +0700

    Update config.js

--- config.js
+++ config.js
@@ -13,7 +13,7 @@ const CONFIG = {
     btnReply: 'Gửi cho anh <3',
     reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
     mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
-    messDesc: 'Mình gặp nhau nhé công chúa.',
+    messDesc: 'Mình gặp nhau nhé công chúa, đồng ý thì nhắn cho anh nhé.',
     btnAccept: 'Okiiiii lun <3',
     messLink: 'https://m.me/larry.namthan' 
 }
